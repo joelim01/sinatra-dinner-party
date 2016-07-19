3dcc7f44381967bc106d17f0865515449004a636
Joseph Lim <joseph.d.lim@gmail.com>
2016-07-19 22:09:56 +0000

    Added db/migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :reservations
   has_many :dinner_dishes
-  has_many :dinners, :through :dinner_dishes
-  has_many :dishes, :through :dinners
+  has_many :dinners, through: :dinner_dishes
+  has_many :dishes, through: :dinners
 end
