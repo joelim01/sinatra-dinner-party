3072d0455b2e75e878202eb99ef0f06bfd54ca70
Joseph Lim <joseph.d.lim@gmail.com>
2016-07-19 22:27:30 +0000

    Added Bcrypt and secure password + user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :comments
   has_many :reservations
   has_many :dinner_dishes
   has_many :dinners, through: :dinner_dishes
   has_many :dishes, through: :dinners
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :password
+  validates_uniqueness_of :email
 end
